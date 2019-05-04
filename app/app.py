42880970a1db51208a452863a8a12dcaf2218559
Guerra Marin <guerramarj@8c859057691c.chop.edu>
2019-05-04 18:15:40 -0400

    inlcuding all the categories

--- app/app.py
+++ app/app.py
@@ -8,51 +8,105 @@ import geocoder
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
-    # dcc.Input(
-    #     id='zipcode',
-    #     placeholder='Enter Zip Code',
-    #     type='text',
-    #     value=''
-    # ),
-    #
+    # zipcode input box
+    dcc.Input(
+        id='zipcode',
+        placeholder='Enter Zip Code',
+        type='text',
+        value=''
+    ),
+    # Item selection menu
     dcc.Dropdown(
         id='item',
         placeholder='Select Item',
         options=[
             {'label': u'Dairy & Eggs', 'value': 'dairy_egg'},
-            {'label': u'Produce', 'value': 'msd'},
-            {'label': u'MEAT & SOY PRODUCTS', 'value': 'ch'},
-            {'label': u'NON-PERISHABLE (Soft Packed)', 'value':'other'}
+            {'label': u'Produce', 'value': 'produce'},
+            {'label': u'MEAT & SOY PRODUCTS', 'value': 'meat_soy'},
+            {'label': u'NON-PERISHABLE (Soft Packed)', 'value':'non_peri_soft'},
+            {'label': u'NON-PERISHABLE (Hard Packed)', 'value':'non_peri_hard'},
+            {'label': u'NON-PERISHABLE (Frozen)', 'value':'non_peri_frozen'}
         ]),
 
-    html.Div(
+    dcc.Checklist(
         id='dairy_eggs',
-        children=[
-        dcc.Checklist(
-            options=[
-                {'label': r'Refrigerated', 'value': 'refrigerated'},
-                {'label': r'Damaged or compromised packaging', 'value': 'damage_compromised_packaged'},
-                {'label': r'Off odor or discoloration', 'value': 'odor_discoloration'},
-                {'label': r'Mold', 'value': 'mold'}
+        options=[
+            {'label': r'Refrigerated', 'value': 'refrigerated'},
+            {'label': r'Damaged or compromised packaging', 'value': 'damage_compromised_packaged'},
+            {'label': r'Off odor or discoloration', 'value': 'odor_discoloration'},
+            {'label': r'Mold', 'value': 'mold'}
             ],
-        values=[],
-        labelStyle={'display': 'inline-block'}
-        )]),
+        values=[]
+    ),
 
-    html.Div(
-        id='dairy_eggs_subcat',
-        children=[
-        dcc.Checklist(
-            options=[
-                {'label': r'Fluid Dairy (Milk, Half and Half, Eggnog, etc)', 'value': 'fluid_dairy'},
-                {'label': r'Yogurt, Sour Cream, Cottage Cheese, Pasta', 'value': 'multi'},
-                {'label': r'Salad, Potato Salad, Coleslaw', 'value':'salads'},
-                {'label': r'Hummus', 'value': 'hummus'},
-                {'label': r'Refrigerated Juice, Perishable Beverages', 'value': 'juice'},
-                {'label': r'Soy Milk', 'value': 'soy_milk'}
-            ],
-            values=[]
-        )]),
+    dcc.Checklist(
+        id='produce',
+        options=[
+            {'label': r'Refrigerated', 'value': 'refrigerated'},
+            {'label': r'80-90% of product good for additional 3-5 days after pick-up', 'value': '89'},
+            {'label': r'Off odor or discoloration', 'value': 'odor'},
+            {'label': r'Mold, fungus, insects, or significant decay', 'value': 'mold_fungus_insects_decay'},
+        ],
+        values=[]
+    ),
+
+    dcc.Checklist(
+        id='meat_soy',
+        options=[
+            {'label': r'Frozen within 24 hours of product date', 'value': 'frozen24'},
+            {'label': r'Accepted up to 180 days after product date', 'value': 'accepted_180'},
+            {'label': r'Defrosted', 'value': 'defrosted'},
+            {'label': r'Severe freezer burn', 'value': 'severe_freezer_burn'},
+            {'label': r'Off odor or discoloration', 'value': 'odor_discoloration'},
+            {'label': r'Bloated package', 'value': 'bloated'},
+        ],
+        values=[]
+    ),
+
+    dcc.Checklist(
+        id='non_peri_soft',
+        options=[
+            {'label': r'Intack packaging', 'value': 'intack'},
+            {'label': r'Punctured packaging', 'value': 'puncture'},
+            {'label': r'Accepted up to 180 days after product date', 'value': 'accepted_180'},
+            {'label': r'Leaking ', 'value': 'leaking'}
+        ],
+        values=[]
+    ),
+
+    dcc.Checklist(
+        id='non_peri_hard',
+        options=[
+            {'label': r'Canned & Jarred products (Soup, Condiments, Vegetables, Fish, Meat)', 'value': 'canned_jarred'},
+            {'label': r'Room temperature', 'value': 'room_temp'},
+            {'label': r'Open, punctured, bulging, leaking, or seriously damaged', 'value': 'open_punctured_bulding_leak'},
+            {'label': r'Broken or chipped glass ', 'value': 'broken_chipped'}
+        ],
+        values=[]
+    ),
+
+    dcc.Checklist(
+        id='non_peri_frozen',
+        options=[
+            {'label': r'frozen', 'value': 'frozen'},
+            {'label': r'Severe freezer burn', 'value': 'freezer_burn'},
+            {'label': r'Seriously damaged', 'value': 'damaged'},
+        ],
+        values=[]
+    ),
+
+    # dcc.Checklist(
+    #     id='dairy_eggs_subcat',
+    #     options=[
+    #         {'label': r'Fluid Dairy (Milk, Half and Half, Eggnog, etc)', 'value': 'fluid_dairy'},
+    #         {'label': r'Yogurt, Sour Cream, Cottage Cheese, Pasta', 'value': 'multi'},
+    #         {'label': r'Salad, Potato Salad, Coleslaw', 'value': 'salads'},
+    #         {'label': r'Hummus', 'value': 'hummus'},
+    #         {'label': r'Refrigerated Juice, Perishable Beverages', 'value': 'juice'},
+    #         {'label': r'Soy Milk', 'value': 'soy_milk'}
+    #     ],
+    #     values=[]
+    # )
 ])
 
 @app.callback(Output(component_id='dairy_eggs', component_property='style'),
@@ -63,15 +117,55 @@ def toggle_container(toggle_value):
     else:
         return {'display': 'none'}
 
-@app.callback(Output(component_id='dairy_eggs_subcat', component_property='style'),
-              [Input(component_id='dairy_eggs', component_property='children')])
+@app.callback(Output(component_id='produce', component_property='style'),
+              [Input(component_id='item', component_property='value')])
 def toggle_container(toggle_value):
-    values = toggle_value[0]['props']['values']
-    print('values = {0}'.format(values))
-    if values == 'refrigerated' and values not in ['damage_compromised_packaged', 'odor_discoloration', 'mold']:
+    if toggle_value == 'produce':
         return {'display': 'block'}
     else:
         return {'display': 'none'}
+
+@app.callback(Output(component_id='meat_soy', component_property='style'),
+              [Input(component_id='item', component_property='value')])
+def toggle_container(toggle_value):
+    if toggle_value == 'meat_soy':
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
+@app.callback(Output(component_id='non_peri_soft', component_property='style'),
+              [Input(component_id='item', component_property='value')])
+def toggle_container(toggle_value):
+    if toggle_value == 'non_peri_soft':
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
+@app.callback(Output(component_id='non_peri_hard', component_property='style'),
+              [Input(component_id='item', component_property='value')])
+def toggle_container(toggle_value):
+    if toggle_value == 'non_peri_hard':
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
+@app.callback(Output(component_id='non_peri_frozen', component_property='style'),
+              [Input(component_id='item', component_property='value')])
+def toggle_container(toggle_value):
+    if toggle_value == 'non_peri_frozen':
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
+# @app.callback(Output(component_id='dairy_eggs_subcat', component_property='style'),
+#               [Input(component_id='dairy_eggs', component_property='values')])
+# def toggle_container(checklist_values):
+#     if 'refrigerated' in checklist_values and \
+#             'damage_compromised_packaged' not in checklist_values and  \
+#             'odor_discoloration' not in checklist_values and 'mold' not in checklist_values:
+#         return {'display': 'block'}
+#     else:
+#         return {'display': 'none'}
 
 def get_lat_long():
     g = geocoder.ip('me')
