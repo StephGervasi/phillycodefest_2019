049bd6e445dc00f859b86eb75b6e9a7540f3de98
navicto <vmr11@pitt.edu>
2019-05-04 21:55:49 -0400

    location pipeline

--- pantry_location_src/pipeline.py
+++ pantry_location_src/pipeline.py
@@ -1,6 +1,6 @@
 __author__ = 'Victor Ruiz, ruizv1@email.chop.edu'
 import pandas as pd
-
+# import googlemaps
 #pd.set_option("display.max_columns", None)
 pd.set_option("mode.chained_assignment", 'raise')
 from pantry_location_src.distance_donnor_pantry import distance_to_pantries, coordinates_from_zip
@@ -18,8 +18,13 @@ def main():
 
     ### sort and get closest 5 pantries
     distances = distances.sort_values('distance', ascending=True).iloc[0:5]
+    closest = (distances.iloc[0].latitude, distances.iloc[0].longitude)
 
-    print(distances)
+    ### create link to gmaps directions
+    url = "https://maps.google.com?saddr={}&daddr={}"
+    directions = url.format(",".join([str(lat), str(long)]), ",".join([str(closest[0]), str(closest[1])]))
+
+    print(directions)
 
 
 if __name__ == '__main__':
